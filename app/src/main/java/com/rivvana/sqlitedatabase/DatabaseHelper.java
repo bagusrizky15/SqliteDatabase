7c3f5d2f2fa9f96e1e958e816639aeab5009665a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-16 23:03:38 +0700

    .

---
+++ app/src/main/java/com/rivvana/sqlitedatabase/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.rivvana.sqlitedatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "myDatabase.db", null, 21);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table if not exists mytable(id integer primary key autoincrement, title text, message text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists mytable");
+        onCreate(db);
+
+    }
+
+    public void insertData(Data data){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("title", data.getTitle());
+        contentValues.put("message", data.getMessage());
+        long mytable = sqLiteDatabase.insert("mytable", null, contentValues);
+        Log.e(TAG, "insertData: "+mytable );
+
+    }
+}
--- app/src/main/java/com/rivvana/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/rivvana/sqlitedatabase/MainActivity.java
@@ -3,12 +3,28 @@ package com.rivvana.sqlitedatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText title,  message;
+    private Button insertData;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+
+        title = findViewById(R.id.title);
+        message = findViewById(R.id.message);
+        insertData = findViewById(R.id.insertData);
+
+        insertData.setOnClickListener(v -> {
+            databaseHelper.insertData(new Data("Hevin", "Cool"));
+        });
     }
 }
